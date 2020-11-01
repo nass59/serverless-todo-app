ff48cbad173044cc2de18b24d2af96c1f5aaf4ef
Nassim Afrete <nassim.afrete@gmail.com>
2020-11-01 16:26:03 +0100

    Init backend

---
+++ backend/src/lambda/http/getTodos.ts
@@ -0,0 +1,13 @@
+import 'source-map-support/register'
+
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda'
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  // TODO: Get all TODO items for a current user
+}
